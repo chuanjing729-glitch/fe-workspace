8eb5b78a4234bcaf81d4546fd4a32d6fe5d0524c
chuanjing.li <chuanjing729@gmail.com>
2025-12-18 04:46:47 +0800

    fix: 修复 webpack-spec-plugin 构建配置并添加 vitest 配置

---
+++ packages/webpack-spec-plugin/vitest.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vitest/config'
+
+export default defineConfig({
+  test: {
+    include: ['test/**/*.js'],
+    exclude: [
+      '**/node_modules/**',
+      '**/dist/**',
+      '**/cypress/**',
+      '**/.{idea,git,cache,output,temp}/**',
+      '**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build,eslint,prettier}.config.*',
+      'test/README.md'
+    ]
+  }
+})
