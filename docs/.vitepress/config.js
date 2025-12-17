c3ca1d6c258520de4304b101bbf254ea6b9a213e
chuanjing729 <chuanjing729@gmail.com>
2025-12-17 08:29:45 +0000

    feat: upgrade docs to VitePress

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,15 @@
+export default {
+  title: 'FE Workspace Docs',
+  description: 'Component and utility documentation.',
+  themeConfig: {
+    sidebar: [
+      {
+        text: 'Introduction',
+        items: [
+          { text: 'What is this?', link: '/' },
+        ]
+      },
+      // ... more sidebar items here in the future
+    ]
+  }
+}
--- docs/README.md
+++
@@ -1,6 +0,0 @@
-# Welcome to the Documentation
-
-This is the main page of your documentation site.
-
-- You can edit this file (`docs/README.md`) to change the content of the homepage.
-- You can add more markdown files in the `docs` directory and link them here.
