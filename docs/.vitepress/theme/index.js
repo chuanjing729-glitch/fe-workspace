21667db595e2c5f9338e8f7a74f43ef7244ad250
chuanjing.li <chuanjing729@gmail.com>
2025-12-19 18:56:13 +0800

    feat: 全局自动为文档添加作者署名 (Chuanjing Li)

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,4 +1,12 @@
+import { h } from 'vue'
 import DefaultTheme from 'vitepress/theme'
 import './custom.css'
 
-export default DefaultTheme
+export default {
+    ...DefaultTheme,
+    Layout() {
+        return h(DefaultTheme.Layout, null, {
+            'doc-before': () => h('div', { class: 'vp-doc-author' }, '作者：Chuanjing Li')
+        })
+    }
+}
